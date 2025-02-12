8f97794748583bb43f7667b8e4c5f4706e05f879
Admin <admin@Rana-Kaleem.local>
2025-02-12 19:57:57 +0500

    basic clerk auth

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,19 @@
+// import { SignUp } from "@clerk/nextjs";
+
+// export default function SignUpPage() {
+//   return (
+//     <div className='h-screen w-full grid place-content-center'>
+//       <SignUp />
+//     </div>
+//   );
+// }
+
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return (
+    <div className='h-screen w-full grid place-content-center'>
+      <SignUp />
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { Inter, Barlow } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 // Fonts
 const interFont = Inter({ subsets: ["latin"] });
 const barlowFont = Barlow({
@@ -27,19 +29,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body
-        className={`${interFont.className} ${barlowFont.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
+    <ClerkProvider>
+      <html lang='en'>
+        <body
+          className={`${interFont.className} ${barlowFont.variable} antialiased`}
         >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
